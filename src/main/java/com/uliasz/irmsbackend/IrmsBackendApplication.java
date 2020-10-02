b8a2e892271b294108b7e1c445008ff04addf4ba
Krzysztof Uliasz <krzysztof.uliasz@gmail.com>
2020-10-02 07:41:34 +0200

    [IRMS-1] Add project template.

---
+++ src/main/java/com/uliasz/irmsbackend/IrmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.uliasz.irmsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrmsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IrmsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uliasz/irmsbackend/IrmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uliasz.irmsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrmsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
